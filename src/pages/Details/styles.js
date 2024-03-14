e4cc7e5eb8376354444cad578ff576de3f4fbcfb
caio <caioeliasvieira2@gmail.com>
2024-03-14 15:10:13 -0300

    Update details

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -91,7 +91,7 @@ export const Content = styled.div`
 
             @media (max-width: 425px) {
                 display: grid;
-                grid-template-columns: repeat(3, 1fr);
+                grid-template-columns: repeat(2, 1fr);
                 gap: 2.4rem;
 
                 margin-left: 2.5rem;
